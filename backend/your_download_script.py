df94ff457d74d680a0f6280fbdb75c60c0c552f1
Tanisha Karmakar <tanisha.ninni@gmail.com>
2024-07-09 00:42:01 +0530

    Update your_download_script.py

--- backend/your_download_script.py
+++ backend/your_download_script.py
@@ -1,14 +1,13 @@
 import boto3
 import os
 
-def download_model_from_s3(bucket_name, model_path, download_path):
+def download_model_from_s3(bucket_name, model_key, download_path):
     s3 = boto3.client('s3')
-    s3.download_file(bucket_name, model_path, download_path)
+    directory = os.path.dirname(download_path)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+    
+    s3.download_file(bucket_name, model_key, download_path)
+    print(f"Model downloaded from S3 bucket {bucket_name} to {download_path}")
 
-bucket_name = 'contentcraftbucket'
-model_path = 'models/7B/llama-2-7b-chat.Q4_K_M.gguf'
-download_path = './models/7B/llama-2-7b-chat.Q4_K_M.gguf'
-
-if not os.path.exists(download_path):
-    os.makedirs(os.path.dirname(download_path), exist_ok=True)
-    download_model_from_s3(bucket_name, model_path, download_path)
