ded6ce2b7a4a1e4c5778a56856ac6bfeb82633f2
Tanisha Karmakar <tanisha.ninni@gmail.com>
2024-07-08 16:30:40 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.2447c96c076e6c2541fc58b7c6f00ee1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3146ecd0e8d3fbd88933972e08f55f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e84193ecb8e4b9664ca",
+    "url": "/static/css/main.643d57f0.chunk.css"
+  },
+  {
+    "revision": "b46943e4f81c0a8d8d41",
+    "url": "/static/js/2.66609e50.chunk.js"
+  },
+  {
+    "revision": "2e84193ecb8e4b9664ca",
+    "url": "/static/js/main.3c2c9f92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d276f3af6d260b83dcf42f2e106f52e",
+    "url": "/static/media/pic2.8d276f3a.jpg"
+  },
+  {
+    "revision": "ea1758075823294db5a6b3c6b9cb9895",
+    "url": "/static/media/pic3.ea175807.jpeg"
+  }
+]);
