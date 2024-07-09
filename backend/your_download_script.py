6d50bd8adcd1e9a0862f950d93a75dc242bbf046
Tanisha Karmakar <tanisha.ninni@gmail.com>
2024-07-09 15:57:16 +0530

    Update your_download_script.py

--- backend/your_download_script.py
+++ backend/your_download_script.py
@@ -16,18 +16,16 @@ def download_model_from_s3(bucket_name, model_key, download_path):
                       aws_secret_access_key=aws_secret_access_key, 
                       region_name=aws_region)
 
+    # Ensure the directory exists
     directory = os.path.dirname(download_path)
-    s3 = boto3.client('s3')
-
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
-    # Ensure the directory exists
-    os.makedirs(os.path.dirname(download_path), exist_ok=True)
 
     # Download the file from S3
     s3.download_file(bucket_name, model_key, download_path)
     print(f"Model downloaded from S3 bucket {bucket_name} to {download_path}")
     print(f"Model downloaded to: {download_path}")
+
 
 
 
