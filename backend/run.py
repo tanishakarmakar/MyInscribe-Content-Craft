58fc904b4f7d8e15057c49c2c0256fd359a67a85
Tanisha Karmakar <tanisha.ninni@gmail.com>
2024-07-09 12:59:41 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,6 @@ import os
 from app import create_app
 from your_download_script import download_model_from_s3
 
-app = create_app()
-
 # Download model from S3 during startup
 bucket_name = 'contentcraftbucket'
 model_key = 'models/7B/llama-2-7b-chat.Q4_K_M.gguf'
@@ -11,6 +9,8 @@ download_path = './models/7B/llama-2-7b-chat.Q4_K_M.gguf'
 
 # Ensure the model is downloaded
 download_model_from_s3(bucket_name, model_key, download_path)
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
