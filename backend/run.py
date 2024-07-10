a79d7df496718d1b1eb0224f7165a877fe1ad6a3
Tanisha Karmakar <tanisha.ninni@gmail.com>
2024-07-10 23:58:56 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render will set the PORT variable
+    port = int(os.environ.get('PORT', 8000))  # Render will set the PORT variable
     app.run(host='0.0.0.0', port=port, debug=True)
 
